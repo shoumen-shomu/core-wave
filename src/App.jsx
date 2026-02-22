ac616a038f2f6f7ff12420cf4c0b98f143054785
shoumen-shomu <shoumenshomu@gmail.com>
2026-02-22 23:45:55 +0600

    lazy added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import { lazy } from 'react';
 import "./App.css";
 import RootLayouts from "./components/layouts/RootLayouts";
-import Home from "./components/pages/home";
+// import Home from "./components/pages/home";
+// import About from "./components/pages/About";
+const About = lazy(() => import('./components/pages/About'));
+const Home = lazy(() => import('./components/pages/home'));
 
 
 
@@ -13,8 +17,8 @@ function App() {
       <Routes>
         <Route path="/" element={<RootLayouts />}>
           <Route index element={<Home />} />
-          {/* <Route path="about" element={<About />} />
-          <Route path="*" element={<Error />} /> */}
+           <Route path="/about" element={<About />} />
+          {/* <Route path="*" element={<Error />} /> */}
         </Route>
       </Routes>
     </>

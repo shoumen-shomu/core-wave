0be48c2ba276f0d3e29c99c98077bf3d7db4eedf
shoumen-shomu <shoumenshomu@gmail.com>
2026-02-26 13:50:49 +0600

    Trail Part End

--- src/components/layouts/Trail.jsx
+++ src/components/layouts/Trail.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Button from "../common/Button";
 import Container from "../common/Container";
+import Images from "../common/Images";
+import women from "../../assets/women.png"
 
 const Trail = () => {
   return (
@@ -8,8 +10,13 @@ const Trail = () => {
       <section>
         <div className="py-[86px] bg-primary ">
           <Container>
-            <h3 className="font-lato font-semibold text-[45px] text-white leadiong-[54px] pb-9.5">Don’t worries, start your free trial today!</h3>
-            <Button className={"!bg-[#2F2F2F]"} btnText={"Get Free Trial"} />
+            <div className="relative">
+              <h3 className="font-lato font-semibold text-[45px] text-white leadiong-[54px] pb-9.5">
+                Don’t worries, start your free trial today!
+              </h3>
+              <Button className={"!bg-[#2F2F2F]"} btnText={"Get Free Trial"} />
+              <Images className={"absolute top-1/2 right-0 -translate-y-1/2"} imgSrc={women}/>
+            </div>
           </Container>
         </div>
       </section>

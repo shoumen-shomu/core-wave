e4b758c412f07382139c80d7a7d25d26233709ba
shoumen-shomu <shoumenshomu@gmail.com>
2026-02-27 21:57:20 +0600

    Refactor footer component: enhance styling and structure for contact details and social media icons

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Container from '../common/Container'
 import Images from '../common/Images'
 import footerlogo from "../../assets/logo.png"
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -11,13 +15,23 @@ const Footer = () => {
       <div className="pt-25 pb-1 flex justify-between">
         <div className="">
           <Images imgSrc={footerlogo}/>
-          <p>mukimsdesign@gmail.com</p>
-          <p>+88 01767630044</p>
-          <div className="flex gap-1">
-            
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-5.5 pt-6.5'>mukimsdesign@gmail.com</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-5.5 pb-1'>+88 01767630044</p>
+          <div className="flex gap-1.75 mt-[20px]">
+            <div className="w-7.25 h-7.25 bg-primary rounded-[50%] flex justify-center items-center">
+              <FaFacebookF size={15} className='text-white' />
+            </div> 
+            <div className="w-7.25 h-7.25 bg-primary rounded-[50%] flex justify-center items-center">
+              <FaTwitter size={15} className='text-white' />
+            </div> 
+            <div className="w-7.25 h-7.25 bg-primary rounded-[50%] flex justify-center items-center">
+              <FaLinkedinIn  ookF size={15} className='text-white' />
+            </div> 
+            <div className="w-7.25 h-7.25 bg-primary rounded-[50%] flex justify-center items-center">
+              <FaInstagram size={15} className='text-white' />
+            </div> 
           </div>
         </div>
-
       </div>
     </Container>
    </footer>

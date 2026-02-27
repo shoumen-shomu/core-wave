3f99cae933cb2ddeffa59a284ba55a94b3dd681a
shoumen-shomu <shoumenshomu@gmail.com>
2026-02-27 22:23:34 +0600

    Add services and help sections to footer component

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -32,6 +32,28 @@ const Footer = () => {
             </div> 
           </div>
         </div>
+        <div className="">
+          <h4 className='font-lato font-semibold text-[20px] text-secendary leading-[30px] pb-[25px]'>Services</h4>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Web Design</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Web Development</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>SEO Marketing</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>UI/UX Analysis</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Digital Marketing</p>
+        </div>
+        <div className="">
+          <h4 className='font-lato font-semibold text-[20px] text-secendary leading-[30px] pb-[25px]'>Help</h4>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Account</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Support Center</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Privacy Policy</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Terms & Conditions</p>
+        </div>
+        <div className="">
+          <h4 className='font-lato font-semibold text-[20px] text-secendary leading-[30px] pb-[25px]'>Conta</h4>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Account</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Support Center</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Privacy Policy</p>
+          <p className='font-lato font-normal text-base text-[#7B7B7B] leading-[22px] pb-[8px]'>Terms & Conditions</p>
+        </div>
       </div>
     </Container>
    </footer>
